9320fd69ca698712bb158f2965af0ff7a7200282
layyanar <layyanar@deloitte.com.au>
2021-06-16 21:47:47 +1000

    updated Env.scala file

--- src/test/java/utils/environment/Environment.scala
+++ src/test/java/utils/environment/Environment.scala
@@ -5,7 +5,7 @@ object Environment {
   val baseUrl = scala.util.Properties.envOrElse("bseUrl", "http://localhost:8080")
   val users = scala.util.Properties.envOrElse("numberOfUsers", "10").toInt
   val maxResponseTime = scala.util.Properties.envOrElse("maxResponseTime", "5000")
-  val duration = scala.util.Properties.envOrElse("duration", "300").toInt
+  val duration = scala.util.Properties.envOrElse("duration", "60").toInt
   // mvn clean test-compile gatling:test -Dgatling.simulationClass=performance.UserSimulation
 
 
